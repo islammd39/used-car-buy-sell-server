22c674a7ab49a96c1cf1d2e221c0ad89583c9a0e
islammd39e <islammdrahat308@gmail.comm>
2022-11-25 21:02:07 +0600

    connect dotenviroment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
@@ -10,7 +11,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rbojs1n.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://<username>:<password>@cluster0.rbojs1n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+// console.log(uri);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
