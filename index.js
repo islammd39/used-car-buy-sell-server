c9b33efb85d63dfac58783c6bbd096eb7d5fc924
islammd39e <islammdrahat308@gmail.comm>
2022-11-25 20:40:14 +0600

    create collection mongodb

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.rbojs1n.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
